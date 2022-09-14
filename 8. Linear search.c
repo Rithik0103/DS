c5413234c5e9682203d4d4199bc74032557fac0b
Rithik0103 <112692646+Rithik0103@users.noreply.github.com>
2022-09-14 21:35:33 +0530

    Update 8. Linear search.c

--- 8. Linear search.c
+++ 8. Linear search.c
@@ -1,70 +1,21 @@
 #include<stdio.h>
 int main()
 {
-	int s[100],n,top,m,i,ans = 1,choice;
-	printf("Enter the size of the Stack : ");
-	if(!(scanf("%d",&n)) || n < 0)
-	{
-		printf("Invalid Input!!!");
-	}
-	else
-	{
-		top = -1;
-		printf("Operations\n1.Push\n2.Pop\n3.Peek\n");
-		while(ans)
-		{
-			printf("\nEnter you Choice : ");
-			scanf("%d",&choice);
-			if(choice == 1)
-			{
-				if(top >= n - 1)
-				{
-					printf("Overflows\n");
-				}
-				else
-				{
-					printf("Enter the Element : ");
-					scanf("%d",&m);
-					top++;
-					s[top] = m;
-				}
-				printf("\n");
-			}
-			else if(choice == 2)
-			{
-				if(top <= -1)
-				{
-					printf("The Stack is Empty!!\n");
-				}
-				else
-				{
-					printf("%d is popped \n",s[top]);
-					top--;
-				}
-			}
-			else if(choice == 3)
-			{
-				printf("\n");
-				if(top >= 0)
-				{
-					for(i = top;i >= 0;i--)
-					{
-						printf("%d\n",s[i]);
-					}
-				}
-				else
-				{
-					printf("The Stack is Empty!!\n");
-				}
-			}
-			else
-			{
-				printf("Invalid choice!!\n");
-			}
-			printf("Do you want to Continue ? ");
-			scanf("%d",&ans);
-			printf("\n");
-		}
-	}
-	return 0;
+    int a[10],i,n,key,flag;
+    printf("enter the n value");
+    scanf("%d",&n);
+    printf("array elements");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    printf("enter the key");
+    scanf("%d",&key);
+    for(i=0;i<n;i++)
+        if(a[i]==key)
+            flag=1;
+        else
+            break;
+    if(flag==1)
+        printf("the element found");
+    else
+        printf("the element not found");
 }
